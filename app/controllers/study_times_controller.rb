2f9095cd2dd57972a8cafb3818127a39d21597b6
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-01-07 05:16:21 +0900

    曜日ごとに学習時間を表示する

--- app/controllers/study_times_controller.rb
+++ app/controllers/study_times_controller.rb
@@ -1,7 +1,13 @@
 class StudyTimesController < ApplicationController
   def index
-    # @study_times = StudyTime.all
-    @study_times = StudyTime.where(date: Date.today)
-    @total_study_time_today = @study_times.sum(:study_time)
+    @study_times = StudyTime.all
+
+    @total_study_time_today = @study_times.where(date: Date.today).sum(:study_time)
+    @total_study_time = @study_times.sum(:study_time)
+
+    # 1週間の学習時間を取得
+    @weekly_study_time = (0..6).map do |i|
+      @study_times.where(date: Date.today.beginning_of_week(:monday) + i.days).sum(:study_time)
+    end
   end
 end
